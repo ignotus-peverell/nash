702ebeaee85521fd372b229f77a9f65c9c1d4379
Hannah Fouasnon <hanelifou@gmail.com>
2017-04-17 17:04:44 -0700

    Pull out graph code from everywhere and isolate it.

--- app/static/js/angular/graph/GraphCtrl.js
+++ app/static/js/angular/graph/GraphCtrl.js
@@ -4,52 +4,6 @@
 
 var nash = angular.module('nash');
 
-var renderGraph = function(graph) {
-    var helpers = graph.helpers;
-    var default_helper = graph.default_helper;
-    var nodes2 = graph.nodes;
-    var edges2 = graph.edges;
-
-    helpers.forEach(function (h) {
-
-        d3.select("#helpers").append("img").attr("src", h.photo)
-            .on("click", function (x) {
-                adopt_view(h);
-            });
-
-        d3.select("#helpers").append("span").text(h.name)
-            .on("click", function (x) {
-                adopt_view(h);
-            });
-    });
-
-    while (nodes.length > 0) {
-        nodes.pop();
-    }
-
-    nodes2.forEach(function (n) {
-        nodes.push(n);
-    });
-
-    while (edges.length > 0) {
-        edges.pop();
-    }
-
-    edges2.forEach(function (l) {
-        edges.push({source: nodes[l.source],
-                    target: nodes[l.target],
-                    detailed: l.detailed,
-                    meaning: l.meaning,
-                    cause_weird: l.cause_weird,
-                    prevent_weird: l.prevent_weird,
-                   });
-    });
-
-    adopt_view(default_helper);
-
-    redraw();
-}
-
 nash.controller('GraphCtrl', [
     '$scope',
     '$routeParams',
@@ -66,7 +20,6 @@ nash.controller('GraphCtrl', [
                 .then(function successCallback(response) {
                     console.log("GET _graph success response: ", response)
                     $scope.graph = response.data.graph;
-                    renderGraph($scope.graph);
                     $scope.graphDataLoaded = true;
                 }, function errorCallback(response) {
                     // TODO: add error handling
@@ -83,6 +36,16 @@ nash.controller('GraphCtrl', [
             console.log($scope.graph)
             api.saveGraph([], $scope.graph)
                 .then(function successCallback(response) {
+                    // var edges2 = [];
+                    // edges.forEach(function (l) {
+                    //     edges2.push({source: l.source.index,
+                    //                  target: l.target.index,
+                    //                  detailed: l.detailed,
+                    //                  meaning: l.meaning,
+                    //                  cause_weird: l.cause_weird,
+                    //                  prevent_weird: l.prevent_weird,
+                    //                 });
+                    //  });
                     console.log("POST _graph success response: ", response)
                     renderGraph($scope.graph);
                 }, function errorCallback(response) {
